bd59de803634582bc18e455a86b9956f978d23d2
meerzaheen_90 <meerzaheen_90@hotmail.com>
2016-05-24 22:06:17 -0400

    attempted to compile "SayHello" method and working on "ShowAge"

--- COMP123-s2016-Lab2/Person.cs
+++ COMP123-s2016-Lab2/Person.cs
@@ -6,7 +6,74 @@ using System.Threading.Tasks;
 
 namespace COMP123_s2016_Lab2
 {
+    /**
+    * This class defines a generic Person
+    *
+    * @class Person
+    * @field _name {string}
+    */
     public class Person
     {
+        // Private Intance variables ++++++++++++++++++++++++++++++++++++
+        private string _name;
+        private int _age;
+
+        // Public properties ++++++++++++++++++++++++++++++++++++++++++++
+
+
+        /**
+        * This is a property for our _name field
+        *
+        * @property {string} Name
+        */
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+
+            set
+            {
+                this._name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this._age;
+            }
+
+            set
+            {
+                this._age = value = 26;
+            }
+        }
+        
+        /**
+        * This is the default empty constructor for the Persons Class
+        *
+        * @constructor Person
+        */
+
+        public Person()
+        {
+            this._name = "Meer";
+        }
+
+        /**
+        * This is a constructor that takes name as a parameter and passes
+        * it to the _name private instance variable
+        *
+        * @constructor Person
+        * @param {string} name
+        */
+
+        public Person(string SayHello)
+        {
+            this._name = SayHello;
+        }
     }
 }
--- COMP123-s2016-Lab2/Program.cs
+++ COMP123-s2016-Lab2/Program.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace COMP123_s2016_Lab2
 {
+    /**
+    * This class is the "driver" class for our Program
+    *
+    * @class Program
+    */
     public class Program
     {
+        /**
+        * the main method for our driver class Program
+        *
+        * @method Main
+        * @param {string[]} args
+        */
         public static void Main(string[] args)
         {
+            Person person;
+
+            // create a new instance for the Person class
+            person = new Person();
+
+            Console.WriteLine(person.Name);
+
+            // set a value in the Name properly of the Person class
+            person.Name = ("Meer");
+
+            // get the Name Value and print to console
+            Console.WriteLine("{0} says hello", person.Name);
         }
     }
 }
