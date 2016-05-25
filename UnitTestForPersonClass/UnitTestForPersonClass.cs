9eb143c83e8ac57b608aba42d2e745f4842d64d3
meerzaheen_90 <meerzaheen_90@hotmail.com>
2016-05-24 21:12:47 -0400

    Added Unit Test Project

---
+++ UnitTestForPersonClass/UnitTestForPersonClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using COMP123_s2016_Lab2;
+
+namespace UnitTestForLab2
+{
+    [TestClass]
+    public class UnitTestForPersonClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
